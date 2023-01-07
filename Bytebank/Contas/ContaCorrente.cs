8ac186b1264dcd3490e87f5b6e01fac1dbe35a23
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2023-01-07 12:36:04 -0300

    Adicionar arquivos de projeto.

---
+++ Bytebank/Contas/ContaCorrente.cs
@@ -0,0 +1,83 @@
+﻿using Bytebank.Titular;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bytebank.Contas
+{
+    public class ContaCorrente
+    {
+        private int numero_agencia;
+        public int Numero_agencia
+        {
+            get
+            {
+                return this.numero_agencia;
+            }
+            set 
+            { 
+                if(value > 0)
+                this.numero_agencia = value;
+            }
+        }
+
+        public string Conta { get; set; }
+
+        private double saldo = 10;
+
+        public Cliente Titular { get; set; }
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (valor <= saldo)
+            {
+                saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (valor > saldo)
+            {
+
+                Console.WriteLine("Voce não possui saldo suficiente");
+                Console.WriteLine("Seu saldo é de: " + saldo);
+                return false;
+            }
+            else
+            {
+                Sacar(valor);
+                destino.Depositar(valor);
+                Console.WriteLine("Transferencia realizada com sucesso");
+                return true;
+            }
+        }
+
+        public void SetSaldo(double valor)
+        {
+            if(valor < 0)
+            {
+                return;
+            }
+            else
+            {
+                this.saldo += valor;
+            }
+        }
+        public double GetSaldo()
+        {
+            return this.saldo;
+        }
+    }
+}
---
+++ Bytebank/Program.cs
@@ -0,0 +1,20 @@
+﻿using Bytebank.Contas;
+using Bytebank.Titular;
+
+Cliente cliente01 = new Cliente();
+cliente01.nome = "Rafael Madeira";
+cliente01.cpf = "4564654645";
+cliente01.profissao = "Analista";
+
+ContaCorrente conta01 = new ContaCorrente();
+conta01.Numero_agencia = 123;
+conta01.Conta = "123123-1";
+
+ContaCorrente conta02 = new ContaCorrente();
+
+
+conta01.SetSaldo(100);
+
+Console.WriteLine(conta01.GetSaldo());
+Console.WriteLine(conta01.Numero_agencia);
+Console.WriteLine(conta01.Conta);
